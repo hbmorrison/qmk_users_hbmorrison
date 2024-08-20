1b6e56ea78f89e3189a123ccc2d07e268260d994
Hannah Morrison <hannah.morrison@ed.ac.uk>
2024-08-20 15:09:13 +0100

    Add layers for capitals and put modifiers behind combos

--- rules.mk
+++ rules.mk
@@ -2,9 +2,13 @@
 
 SRC += hbmorrison.c
 
+# This is a bit of a hack to allow combos to be defined in userspace.
+
+INTROSPECTION_KEYMAP_C = hbm_combos.c
+
 # Enabled features.
 
 SEND_STRING_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
-TAP_DANCE_ENABLE = yes
